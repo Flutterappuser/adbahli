b17c91cf35baac74496ab8283a9511cd81a9094f
Flutterappuser <programeur2017@gmail.com>
2023-05-15 03:04:33 +0100

    add possibility to control the button

--- lib/controller/onboarding_controller.dart
+++ lib/controller/onboarding_controller.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
+
+import '../data/datasource/static/static.dart';
 
 abstract class OnBoardingController extends GetxController {
   next();
@@ -6,13 +9,30 @@ abstract class OnBoardingController extends GetxController {
 }
 
 class OnBoardingControllerImp extends OnBoardingController {
+  late PageController pageController;
   int current_page = 0;
   @override
-  next() {}
+  next() {
+    current_page++;
+    if (current_page > onBoardingList.length - 1) {
+      current_page = 0;
+      pageController.animateToPage(current_page,
+          duration: Duration(milliseconds: 500), curve: Curves.ease);
+    } else {
+      pageController.animateToPage(current_page,
+          duration: Duration(milliseconds: 500), curve: Curves.ease);
+    }
+  }
 
   @override
   onPageChanged(int index) {
     current_page = index;
     update();
+  }
+
+  @override
+  void onInit() {
+    pageController = PageController();
+    super.onInit();
   }
 }
--- lib/view/widget/onboarding/custombutton.dart
+++ lib/view/widget/onboarding/custombutton.dart
@@ -1,21 +1,27 @@
+import 'package:adbahli/controller/onboarding_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../../../core/constant/color.dart';
 
-class CustomButtonOnBoarding extends StatelessWidget {
+class CustomButtonOnBoarding extends GetView<OnBoardingControllerImp> {
   const CustomButtonOnBoarding({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(bottom: 30),
-      height: 40,
-      child: MaterialButton(
-          padding: const EdgeInsets.symmetric(horizontal: 100, vertical: 0),
-          textColor: Colors.white,
-          onPressed: () {},
-          color: AppColor.primaryColor,
-          child: const Text("Continue")),
+    return GetBuilder<OnBoardingControllerImp>(
+      builder: (controller) => Container(
+        margin: const EdgeInsets.only(bottom: 30),
+        height: 40,
+        child: MaterialButton(
+            padding: const EdgeInsets.symmetric(horizontal: 100, vertical: 0),
+            textColor: Colors.white,
+            onPressed: () {
+              controller.next();
+            },
+            color: AppColor.primaryColor,
+            child: const Text("Continue")),
+      ),
     );
   }
 }
